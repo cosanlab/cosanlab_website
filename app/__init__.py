3870488800aa9441b6ef43474b6125f9b36f5b4d
Luke Chang <lukejchang@gmail.com>
2016-12-11 14:57:28 -0500

    updated keys

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import imp
+
+basedir = '/home/lukcha5/cosanlab'
+keypath = imp.load_source('keys', os.path.join(basedir,'keys','keys.py'))
+keys = keypath.Keys()
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.secret_key = keys.secret_key
+app.config['SQLALCHEMY_DATABASE_URI'] = keys.SQLALCHEMY_DATABASE_URI
+# app.config.from_object('config')
 db = SQLAlchemy(app)
 
 
