11cbfee309f025a74ef8cee82e5f8a611fee706b
Luke Chang <lukejchang@gmail.com>
2016-12-11 15:41:51 -0500

    updated basepath

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask_script import Shell, Manager
 import imp
 
-basedir = '/Users/lukechang/Github/cosanlab_web'
-# basedir = '/home/lukcha5/cosanlab'
+# basedir = '/Users/lukechang/Github/cosanlab_web'
+basedir = '/home/lukcha5/cosanlab'
 keypath = imp.load_source('keys', os.path.join(basedir,'keys.py'))
 keys = keypath.Keys()
 
