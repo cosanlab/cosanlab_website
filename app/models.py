9ed507da5106dc93a34103bbf039433a5e967ae7
ljchang <luke.j.chang@dartmouth.edu>
2019-02-08 13:28:52 -0500

    updated database

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+# from sqlalchemy.orm import relationship
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
@@ -11,53 +12,37 @@ class User(db.Model):
 	title = db.Column(db.String(64))
 	bio = db.Column(db.String(1000))
 	pic = db.Column(db.String(120))
-	email = db.Column(db.String(120), unique=True)
-	website = db.Column(db.String(120))
+	email = db.Column(db.String(120), unique=True, nullable=False)
+	website = db.Column(db.String(120), nullable=False)
 	role = db.Column(db.SmallInteger, default=ROLE_USER)
-	cv = db.Column(db.String(120))
+	cv = db.Column(db.String(120), nullable=False)
 
-	#pwdhash = db.Column(db.String(64)
-	#posts = db.relationship('Post', backref = 'user_id', lazy = 'dynamic')
 	def __repr__(self):
 		return '<User %r>' % (self.nickname)
 
-	#def __init__(self, ):
-	#	self.nickname
-	#	self.email = email.lower()
-	#	self.set_password(password)
+class Tags(db.Model):
+	__tablename__ = 'tags'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(300))
+	def __repr__(self):
+		return '<Tags %r>' % (self.name)
 
-	#def is_authenticated(self):
-	#	return True
-
-	#def is_active(self):
-	#	return True
-
-	#def is_anonymous(self):
-	#	return False
-
-	#def get_id(self):
-	#	return unicode(self.id)
-
+tagmap = db.Table('tagmap',
+	db.Column('paper_id', db.Integer, db.ForeignKey('papers.id'), primary_key=True),
+	db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+)
 
 class Papers(db.Model):
 	__tablename__ = 'papers'
 	id = db.Column(db.Integer, primary_key=True)
 	nickname = db.Column(db.String(64), index=True, unique=True)
-	citation = db.Column(db.String(500), index=True, unique=True)
-	link = db.Column(db.String(300), index=True, unique=True)
-	abstract = db.Column(db.String(1000), index = True)
+	citation = db.Column(db.String(300))
+	link = db.Column(db.String(300))
+	abstract = db.Column(db.String(500), nullable=False)
 	year = db.Column(db.Integer)
-	code = db.Column(db.String(300), index=True, unique=True)
-	data = db.Column(db.String(300), index=True, unique=True)
-
+	code = db.Column(db.String(300), nullable=False)
+	data = db.Column(db.String(300), nullable=False)
+	tags = db.relationship('Tags', secondary=tagmap, lazy='subquery',
+		backref=db.backref('paper', lazy=True))
 	def __repr__(self):
 		return '<Papers %r>' % (self.nickname)
-
-#class Post(db.Model):
-#	id = db.Column(db.Integer, primary_key = True)
-#	body = db.Column(db.String(140))
-#	timestamp = db.Column(db.DateTime)
-#	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-#
-#	def __repr__(self):
-#		return '<Post %r>' % (self.body)
