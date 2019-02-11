aa37ca5b97c87ae83854428f37654392450cf1a8
ljchang <luke.j.chang@dartmouth.edu>
2019-02-11 11:15:01 -0500

    updated path

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ from flask_script import Shell, Manager
 from flask_migrate import Migrate
 import imp
 
-basedir = '/Users/lukechang/Github/cosanlab_web'
-# basedir = '/home/lukcha5/cosanlab'
+# basedir = '/Users/lukechang/Github/cosanlab_web'
+basedir = '/home/lukcha5/cosanlab'
 keypath = imp.load_source('keys', os.path.join(basedir,'keys.py'))
 keys = keypath.Keys()
 
